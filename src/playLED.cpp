4fa4f18a0a057457b2e4ea4ea0bdfcf9d5d813c2
Raymond Lan <rlan06@students.cs.ubc.ca>
2026-09-19 15:23:47 -0700

    Added comments for LED

--- src/playLED.cpp
+++ src/playLED.cpp
@@ -1,3 +1,11 @@
+/*
+
+Functions relating to Arduino Nano ESP32S3's on-board LED.  Below are some of the types of LED visuals that I have used
+
+The pin works in a LOW setting, so powerLow() so  writing the pin to low turns on the LED, and vise versa
+
+*/
+
 #include "playLED.h"
 #include <Arduino.h>
 
@@ -24,6 +32,8 @@ void ledOff() {
     powerHigh();
 }
 
+// blinks the LED n times (depending on count)
+
 static void blinkCount(int count) {
     for (int i = 0; i < count; i++) {
         powerLow();
@@ -33,12 +43,18 @@ static void blinkCount(int count) {
     }
 }
 
+// number of blinks when connnction to the network was successful
+
 void blinkSuccess() {
     blinkCount(4);
 }
+
+// number of blinks when connnction to the network has failed
 void blinkFailed()  {
     blinkCount(2);
 }
+
+// number of blinks when connnction to the network is retrying / attempting to connect  NOTE: The blinking on initialization is the only time it does not work.
 void blinkAttemptConnect() {
     powerLow();
     delay(500);
