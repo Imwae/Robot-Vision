2780f473fbda7f325e8a123ff6161c34058979ca
Raymond Lan <rlan06@students.cs.ubc.ca>
2026-09-20 16:08:49 -0700

    Added HTML text to HTTP

--- src/webManager.cpp
+++ src/webManager.cpp
@@ -19,11 +19,97 @@ static AsyncWebSocket ws("/ws");
 static char storedCharacter = '\0';
 static unsigned long lastMessageTime = 0;
 
+static const char *htmlContent PROGMEM = R"rawliteral(
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>RobotMovement</title>
+        </head>
+        <body>
+
+        <p id="textSpace">
+            Control movement: 
+
+        </p>
+
+        <button type="button" id="toggleButton">
+            Control movement toggle
+        </button>
+
+        <button type="button" id="retryConnection">
+            Retry connection
+        </button>
+
+        <script>
+            let keyInput = ""
+            let controlEnabled = false
+            const toggleButton = document.getElementById("toggleButton")
+            const textStatus = document.getElementById("textSpace")
+            const retryConnection = document.getElementById("retryConnection")
+            const primaryHost = window.location.hostname
+            const fallbackIp= "10.0.0.71"
+            const host = primaryHost || fallbackIp
+            let ws
+            connectSocket()
+
+            function connectSocket() {
+                ws = new WebSocket("ws://" + host + "/ws")
+
+                ws.onclose = function showButton() {
+                    retryConnection.style.display = "block"
+                }
+
+                ws.onopen = function hideButton() {
+                    retryConnection.style.display = "none"
+                }
+            }
+
+            setInterval(function() {
+                if ((keyInput !== "") && (ws.readyState === WebSocket.OPEN)) {
+                    console.log(keyInput) 
+                    ws.send(keyInput)
+                }
+                
+            }, 100)
+
+            toggleButton.addEventListener("click", function(event) {
+                controlEnabled = !controlEnabled
+                textStatus.textContent = "Control movement toggle: " + controlEnabled
+
+                if (!controlEnabled) {
+                    keyInput = ""
+                }
+            })
+
+            retryConnection.addEventListener("click", function(event) {
+                connectSocket()
+            })
+
+            document.addEventListener("keydown", function(event) {
+                if (controlEnabled) {
+                    keyInput = (event.key).toUpperCase()
+                }
+            })
+
+            document.addEventListener("keyup", function(event) {
+                if (keyInput === (event.key).toUpperCase()) {
+                    keyInput = ""
+                    console.log("Keypress ended!")
+                }
+            })
+
+        </script>
+        </body>
+    </html>
+
+)rawliteral";
+
 // Setup for HTTP Server and WebSocket. Detects key input, and stores the last timestamp that it was received.
 void setupWebPage() {
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        request->send(200, "text/html", "<h1>hello</h1>");
+        request->send(200, "text/html", htmlContent);
     });
 
     ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
